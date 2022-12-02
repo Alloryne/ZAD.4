5166fd059acd057c221415247109699287b62be7
Łukasz Łopacki <ll439936@students.mimuw.edu.pl>
2022-12-02 23:18:59 +0100

    encounter

--- organism.h
+++ organism.h
@@ -3,81 +3,86 @@
 #include <concepts>
 #include <optional>
 #include <tuple>
+#include <cstdint>
+
+// TODO
+// encounter_series()
+// Przerobienie żeby więcej używać szablonów???
 
 template <bool sp1_eats_m, bool sp1_eats_p, bool sp2_eats_m, bool sp2_eats_p>
-concept two_plants_cant_meet = 
-    sp1_eats_m   
-    || sp1_eats_p 
-    || sp2_eats_m 
-    || sp2_eats_p;
+concept two_plants_cant_meet =
+sp1_eats_m
+|| sp1_eats_p
+|| sp2_eats_m
+|| sp2_eats_p;
 
-template <typename species_t, bool can_eat_meat, bool can_eat_plants> 
+template <typename species_t, bool can_eat_meat, bool can_eat_plants>
 requires std::equality_comparable<species_t>
 class Organism {
-    private:
-        uint64_t vitality;
-        species_t species;
-    public:
+private:
+    uint64_t vitality;
+    species_t species;
+public:
     // Constructors
-        constexpr Organism(species_t const &species, uint64_t vitality)
-         : species(species), vitality(vitality) {}
+    constexpr Organism(species_t const &species, uint64_t vitality)
+            : species(species), vitality(vitality) {}
 
-    // Accessors 
-        constexpr uint64_t get_vitality() const {
-            return vitality;
-        }
+    // Accessors
+    constexpr const uint64_t get_vitality() const {
+        return vitality;
+    }
 
-        constexpr species_t &get_species() const {
-            return species;
-        }
+    constexpr const species_t &get_species() const {
+        return species;
+    }
 
     // Methods defined by us for use in the encounter() method.
     // Returns true if an organism is dead.
-        constexpr bool is_dead() const {
-            return (vitality == 0);
-        }
+    constexpr const bool is_dead() const {
+        return (vitality == 0);
+    }
 
     // Returns true if the organism.
-        constexpr bool plant() const {
-            return (!(can_eat_meat || can_eat_plants));
-        }
+    constexpr const bool plant() const {
+        return (!(can_eat_meat || can_eat_plants));
+    }
 
     // Returns true if this organism can eat organism given.
     // Determined by specialization, default is Omnivore.
-        template <
-            typename species_t_other, 
-            bool can_eat_meat_other, 
+    template <
+            typename species_t_other,
+            bool can_eat_meat_other,
             bool can_eat_plants_other
-        > 
-        constexpr const bool can_I_eat(
+    >
+    constexpr const bool can_I_eat(
             Organism<
-            species_t_other, 
-            can_eat_meat_other, 
-            can_eat_plants_other
+                    species_t_other,
+                    can_eat_meat_other,
+                    can_eat_plants_other
             > organism
-        ) {
-            return (
-                can_eat_meat && !organism.plant() 
+    ) {
+        return (
+                can_eat_meat && !organism.plant()
                 || can_eat_plants && organism.plant()
-            );
-        }
+        );
+    }
 
     // Returns an object representing an organism after an encounter.
-        constexpr const Organism<
+     constexpr const Organism<
             species_t,
             can_eat_meat,
             can_eat_plants
-        > after(uint64_t new_vitality) {
-            return Organism(species, new_vitality);
-        }
-    // By default it will return a copy of the organism.
-        constexpr const Organism<
+    > after(uint64_t new_vitality) {
+        return Organism(species, new_vitality);
+    }
+    // By default, it will return a copy of the organism.
+    constexpr const Organism<
             species_t,
             can_eat_meat,
             can_eat_plants
-        > after() {
-            return Organism(species, vitality);
-        }
+    > after() {
+        return Organism(species, vitality);
+    }
 };
 
 // This might be actually fine?
@@ -96,40 +101,40 @@ using Omnivore = Organism<species_t, true, true>;
 namespace {
     // Function determining whether two organisms can have sex
     // in point 4.
-    template < 
-        typename species_t, 
-        bool sp1_eats_m, 
-        bool sp1_eats_p, 
-        bool sp2_eats_m, 
-        bool sp2_eats_p
+    template <
+            typename species_t,
+            bool sp1_eats_m,
+            bool sp1_eats_p,
+            bool sp2_eats_m,
+            bool sp2_eats_p
     > // template
     constexpr const bool // return type
     sex(
-        Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
-        Organism<species_t, sp2_eats_m, sp2_eats_p> organism2
+            Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
+            Organism<species_t, sp2_eats_m, sp2_eats_p> organism2
     ) /*arguments*/ {
-        return ( 
-            organism1.get_species() == organism2.get_species() 
-            && sp1_eats_m == sp2_eats_m
-            && sp1_eats_p == sp2_eats_p
+        return (
+                organism1.get_species() == organism2.get_species()
+                && sp1_eats_m == sp2_eats_m
+                && sp1_eats_p == sp2_eats_p
         );
     } // function body
 
     // Function determining the outcome of an encounter in points 5, 6, 7, 8.
-    template < 
-        typename species_t, 
-        bool sp1_eats_m, 
-        bool sp1_eats_p, 
-        bool sp2_eats_m, 
-        bool sp2_eats_p
+    template <
+            typename species_t,
+            bool sp1_eats_m,
+            bool sp1_eats_p,
+            bool sp2_eats_m,
+            bool sp2_eats_p
     > // template
     constexpr const std::tuple<
-    Organism<species_t, sp1_eats_m, sp1_eats_p>,
-    Organism<species_t, sp2_eats_m, sp2_eats_p>
+            Organism<species_t, sp1_eats_m, sp1_eats_p>,
+            Organism<species_t, sp2_eats_m, sp2_eats_p>
     > // return type
     eating_each_other(
-        Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
-        Organism<species_t, sp2_eats_m, sp2_eats_p> organism2
+            Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
+            Organism<species_t, sp2_eats_m, sp2_eats_p> organism2
     ) /*arguments*/ {
         bool can1eat2 = organism1.can_I_eat(organism2);
         bool can2eat1 = organism2.can_I_eat(organism1);
@@ -137,75 +142,75 @@ namespace {
         uint64_t v2 = organism2.get_vitality();
         // Organism1 attempts to eat organism2.
         if (can1eat2) {
-            if (organism2.plant()) 
-                return make_tuple(organism1.after(v1 + v2), organism2.after(0));
+            if (organism2.plant())
+                return std::make_tuple(organism1.after(v1 + v2), organism2.after(0));
             if (v1 > v2)
-                return make_tuple(organism1.after(v1 + v2/2), organism2.after(0));
+                return std::make_tuple(organism1.after(v1 + v2/2), organism2.after(0));
         }
         // It has failed.
         // Now organism2 attempts to eat organism1.
         if (can2eat1) {
-            if (organism1.plant()) 
-                return make_tuple(organism1.after(0), organism2.after(v1 + v2));
-            if (v2 > v1) 
-                return make_tuple(organism1.after(0), organism2.after(v1/2 + v2));
+            if (organism1.plant())
+                return std::make_tuple(organism1.after(0), organism2.after(v1 + v2));
+            if (v2 > v1)
+                return std::make_tuple(organism1.after(0), organism2.after(v1/2 + v2));
         }
-        // Organisms can't eat each other, they either 
-        // kill each other or do nothing 
+        // Organisms can't eat each other, they either
+        // kill each other or do nothing
         if (can1eat2 && can2eat1)
-            return make_tuple(organism1.after(0), organism2.after(0));
+            return std::make_tuple(organism1.after(0), organism2.after(0));
 
-        return make_tuple(organism1.after(), organism2.after());
+        return std::make_tuple(organism1.after(), organism2.after());
     } // function body
 
 }
 
-template < 
-    typename species_t, 
-    bool sp1_eats_m, 
-    bool sp1_eats_p, 
-    bool sp2_eats_m, 
-    bool sp2_eats_p
+template <
+        typename species_t,
+        bool sp1_eats_m,
+        bool sp1_eats_p,
+        bool sp2_eats_m,
+        bool sp2_eats_p
 > // template
 requires two_plants_cant_meet<
-    sp1_eats_m, 
-    sp1_eats_p, 
-    sp2_eats_m, 
-    sp2_eats_p
+        sp1_eats_m,
+        sp1_eats_p,
+        sp2_eats_m,
+        sp2_eats_p
 > // Two plants can't meet this requirement.
 constexpr const std::tuple<
-    Organism<species_t, sp1_eats_m, sp1_eats_p>,
-    Organism<species_t, sp2_eats_m, sp2_eats_p>,
-    std::optional<Organism<species_t, sp1_eats_m, sp1_eats_p>>
+        Organism<species_t, sp1_eats_m, sp1_eats_p>,
+        Organism<species_t, sp2_eats_m, sp2_eats_p>,
+        std::optional<Organism<species_t, sp1_eats_m, sp1_eats_p>>
 > // return type
 encounter(
-    Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
-    Organism<species_t, sp2_eats_m, sp2_eats_p> organism2
+        Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
+        Organism<species_t, sp2_eats_m, sp2_eats_p> organism2
 ) {
-    if (organism1.is_dead() || organism2.is_dead()) 
-        return make_tuple(organism1.after(), organism2.after(), std::nullopt);
+    if (organism1.is_dead() || organism2.is_dead())
+        return std::make_tuple(organism1.after(), organism2.after(), std::nullopt);
 
-    if (sex(organism1, organism2)) 
-        return make_tuple(
-            organism1.after(), 
-            organism2.after(), 
-            organism1.after(
-                (organism1.get_vitality() + organism2.get_vitality())/2
-            )
+    if (sex(organism1, organism2))
+        return std::make_tuple(
+                organism1.after(),
+                organism2.after(),
+                organism1.after(
+                        (organism1.get_vitality() + organism2.get_vitality())/2
+                )
         );
-    
+
     auto eating_result = eating_each_other(organism1, organism2);
-    return make_tuple(
-        get<0>(eating_result),
-        get<1>(eating_result),
-        std::nullopt
-    );  
+    return std::make_tuple(
+            get<0>(eating_result),
+            get<1>(eating_result),
+            std::nullopt
+    );
 }
 
 // template <
-//     typename species_t, 
-//     bool sp1_eats_m, 
-//     bool sp1_eats_p, 
+//     typename species_t,
+//     bool sp1_eats_m,
+//     bool sp1_eats_p,
 //     typename ... Args
 // > // template
 // constexpr Organism<species_t, sp1_eats_m, sp1_eats_p> // return type
